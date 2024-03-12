a6bb14c64057e888b10ad0832d7c939c24ddc85d
FLynn <1845797+ling7334@users.noreply.github.com>
2024-03-12 14:32:30 +0800

    fix: gmail send with ssl

--- pkg/mail/mail.go
+++ pkg/mail/mail.go
@@ -143,7 +143,8 @@ func SendUserInvitation(to string, magicLink string) error {
 		return ex.ErrEmailFailed
 	}
 
-	err = SendMailWithTLS(u, auth, port, from, to, body.Bytes())
+	err = smtp.SendMail(u.Host+":"+port, auth, from, []string{to}, body.Bytes())
+	// err = SendMailWithTLS(u, auth, port, from, to, body.Bytes())
 	if err != nil {
 		log.Error().Err(err).Msg("fail to send mail")
 		return ex.ErrSenderEmailInvalid
